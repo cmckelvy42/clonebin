b77628b0fe56a4d76bd3379925cc2ba4c8082a45
Chris McKelvy <cmckelvy@pop-os.localdomain>
2022-02-23 15:21:36 -0800

    updated footer links

--- app/javascript/components/Footer/footer.jsx
+++ app/javascript/components/Footer/footer.jsx
@@ -4,8 +4,8 @@ import { GithubIcon, LinkedinIcon } from "../../util/fontawesome_icons";
 
 export const Footer = () => {
     return <footer>
-         <Link to="http://github.com/cmckelvy42/clonebin"><GithubIcon/></Link>
-         <Link to="https://www.linkedin.com/in/cmckelvy/"><LinkedinIcon/></Link>
+         <a href="http://github.com/cmckelvy42/clonebin"><GithubIcon/></Link>
+         <a href="https://www.linkedin.com/in/cmckelvy/"><LinkedinIcon/></Link>
     </footer>
 }
 
